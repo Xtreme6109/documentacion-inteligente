f61b145cb3275952f0f21cb514cd5853bd7e0314
Xtreme6109 <148501441+Xtreme6109@users.noreply.github.com>
2025-05-28 09:10:20 -0600

    Subida de datos básicos

--- DocumentacionInteligente.BackEnd/DocumentacionInteligente.BackEnd/obj/Debug/net9.0/DocumentacionInteligente.BackEnd.AssemblyInfo.cs
+++ DocumentacionInteligente.BackEnd/DocumentacionInteligente.BackEnd/obj/Debug/net9.0/DocumentacionInteligente.BackEnd.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DocumentacionInteligente.BackEnd")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83948c79cbc6387edd239f83285a5b3d072d38ca")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e80f275f94c7e3d4f227997b934ba77e3780b9a7")]
 [assembly: System.Reflection.AssemblyProductAttribute("DocumentacionInteligente.BackEnd")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DocumentacionInteligente.BackEnd")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
